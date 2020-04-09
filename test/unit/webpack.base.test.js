12f733c91890183ef89f632d2fb8ce1d0dcba0e4
yanan.zhang <juc8548@dingtalk.com>
2020-04-09 17:01:34 +0800

    third commit

--- test/unit/webpack.base.test.js
+++ test/unit/webpack.base.test.js
@@ -4,6 +4,6 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
 
     it('entry', () => {
-        assert.equal(baseConfig.entry.search, 'D:/CRRCDT/Study/webpack-demo/build-webpack/test/smoke/template/src/search/index.jsx');
+        assert.equal(baseConfig.entry.search.indexOf('build-webpack/test/smoke/template/src/search/index.jsx') > -1, true);
     })
 })
