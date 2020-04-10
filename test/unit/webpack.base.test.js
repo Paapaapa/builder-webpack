31551915125f835c705f8df2c5c6729c3d848e34
yanan.zhang <juc8548@dingtalk.com>
2020-04-10 10:02:14 +0800

    hhhh

--- test/unit/webpack.base.test.js
+++ test/unit/webpack.base.test.js
@@ -4,6 +4,6 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
 
     it('entry', () => {
-        assert.equal(baseConfig.entry.search.indexOf('build-webpack/test/smoke/template/src/search/index.jsx') > -1, true);
-    })
+         assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.jsx') > -1, true);
+    });
 })
